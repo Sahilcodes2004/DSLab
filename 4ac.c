5ca140ae8830f52da8c964be811c57a30a8329f5
Sahilcodes2004 <sahiljackson86@gmail.com>
2025-10-15 23:31:26 +0530

    changes are done in c

--- 4ac.c
+++ 4ac.c
@@ -1,147 +1,176 @@
 #include <stdio.h>
-#include <alloc.h>
+#include <stdlib.h>
 
-struct Node
-{
+struct Node {
     int info;
-    Node *next;
+    struct Node* next;
 };
-// *head;
 
-// struct Node *head;
-
-struct Node *getNode(int data)
-{
-    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+// Create a new node
+struct Node* getNode(int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
     temp->info = data;
+    temp->next = NULL;
     return temp;
 }
 
-void traverse(Node *head)
-{
-    if (head == NULL)
-    {
-        cout << "Empty List";
+// Display the linked list
+void traverse(struct Node* head) {
+    if (head == NULL) {
+        printf("Empty List\n");
         return;
     }
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->info << " ";
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->info);
         temp = temp->next;
     }
-    cout << endl;
+    printf("\n");
 }
 
-void insertBeg(struct Node **head, int info)
-{
-    Node *newNode = getNode(info);
-    newNode->next = head;
-    head = newNode;
+// Insert at beginning
+void insertBeg(struct Node** head, int info) {
+    struct Node* newNode = getNode(info);
+    newNode->next = *head;
+    *head = newNode;
 }
-void insertLast(Node *&head, int info)
-{
-    Node *newNode = new Node(info);
-    if (head == nullptr)
-    {
-        head = newNode;
+
+// Insert at end
+void insertLast(struct Node** head, int info) {
+    struct Node* newNode = getNode(info);
+    if (*head == NULL) {
+        *head = newNode;
         return;
     }
-    Node *temp = head;
-    while (temp->next != nullptr)
+    struct Node* temp = *head;
+    while (temp->next != NULL)
         temp = temp->next;
     temp->next = newNode;
 }
 
-Node *search(Node *&head, int data)
-{
-    Node *temp = head;
-    while (temp != nullptr)
-    {
+// Search node by value
+struct Node* search(struct Node* head, int data) {
+    struct Node* temp = head;
+    while (temp != NULL) {
         if (temp->info == data)
             return temp;
         temp = temp->next;
     }
-    return nullptr;
+    return NULL;
 }
 
-void insertAfter(Node *&head, int data, int info)
-{
-    Node *temp = search(head, data);
-    if (temp == nullptr)
-    {
-        cout << data << " not found" << endl;
+// Insert after a given node
+void insertAfter(struct Node* head, int afterData, int info) {
+    struct Node* temp = search(head, afterData);
+    if (temp == NULL) {
+        printf("%d not found\n", afterData);
         return;
     }
-    Node *newNode = new Node(info);
+    struct Node* newNode = getNode(info);
     newNode->next = temp->next;
     temp->next = newNode;
 }
 
-void removeBeg(Node *&head)
-{
-}
-void removeLast(Node *&head)
-{
-}
-void removeAfter(Node *&head, Node *current)
-{
+// Remove from beginning
+void removeBeg(struct Node** head) {
+    if (*head == NULL) return;
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
 }
 
-int main()
-{
-    struct Node *head = NULL;
+// Remove from end
+void removeLast(struct Node** head) {
+    if (*head == NULL) return;
+    if ((*head)->next == NULL) {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next->next != NULL)
+        temp = temp->next;
+    free(temp->next);
+    temp->next = NULL;
+}
 
-    int ch, info;
-    do
-    {
-        cout << "1.........Add Begining" << endl
-             << "2.........Add Last" << endl
-             << "3.........Add After" << endl
-             << "4.........Remove Begining" << endl
-             << "5.........Remove Last" << endl
-             << "6.........Remove After" << endl
-             << "7.........Search" << endl
-             << "8.........Show" << endl
-             << "9.........Exit" << endl
-             << "Enter your choice: ";
-        cin >> ch;
-        switch (ch)
-        {
-        case 1:
-            cout << "Enter info: ";
-            cin >> info;
-            insertBeg(&head, info);
-            break;
-        case 2:
-            cout << "Enter info: ";
-            cin >> info;
-            cout << "unimplementd" << endl;
-            break;
-        case 3:
-            cout << "unimplementd" << endl;
-            break;
-        case 4:
-            cout << "unimplementd" << endl;
-            break;
-        case 5:
-            cout << "unimplementd" << endl;
-            break;
-        case 6:
-            cout << "unimplementd" << endl;
-            break;
-        case 7:
-            cout << "unimplementd" << endl;
-            break;
-        case 8:
-            traverse(head);
-            break;
-        case 9:
-            cout << "See you later" << endl;
-            break;
-        default:
-            cout << "Not defined yet" << endl;
+// Remove after a given node
+void removeAfter(struct Node* head, int afterData) {
+    struct Node* temp = search(head, afterData);
+    if (temp == NULL || temp->next == NULL) {
+        printf("Cannot remove after %d\n", afterData);
+        return;
+    }
+    struct Node* toDelete = temp->next;
+    temp->next = temp->next->next;
+    free(toDelete);
+}
+
+int main() {
+    struct Node* head = NULL;
+    int ch, info, afterData;
+
+    do {
+        printf("\n1.........Add Beginning"
+               "\n2.........Add Last"
+               "\n3.........Add After"
+               "\n4.........Remove Beginning"
+               "\n5.........Remove Last"
+               "\n6.........Remove After"
+               "\n7.........Search"
+               "\n8.........Show"
+               "\n9.........Exit"
+               "\nEnter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                printf("Enter info: ");
+                scanf("%d", &info);
+                insertBeg(&head, info);
+                break;
+            case 2:
+                printf("Enter info: ");
+                scanf("%d", &info);
+                insertLast(&head, info);
+                break;
+            case 3:
+                printf("Enter value after which to insert: ");
+                scanf("%d", &afterData);
+                printf("Enter info: ");
+                scanf("%d", &info);
+                insertAfter(head, afterData, info);
+                break;
+            case 4:
+                removeBeg(&head);
+                break;
+            case 5:
+                removeLast(&head);
+                break;
+            case 6:
+                printf("Enter value after which to remove: ");
+                scanf("%d", &afterData);
+                removeAfter(head, afterData);
+                break;
+            case 7:
+                printf("Enter value to search: ");
+                scanf("%d", &info);
+                if (search(head, info) != NULL)
+                    printf("%d found in the list.\n", info);
+                else
+                    printf("%d not found.\n", info);
+                break;
+            case 8:
+                traverse(head);
+                break;
+            case 9:
+                printf("See you later\n");
+                break;
+            default:
+                printf("Not defined yet\n");
         }
     } while (ch != 9);
-    cout<<"This program is created by Sahil Prakash Jackson"<<endl;
+
+    printf("This program is created by Sahil Prakash Jackson\n");
+    return 0;
 }
